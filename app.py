72f5cb3c9759521d555b0ced497b5499c192a322
MathewOwen <m.owen@live.com>
2024-12-04 09:28:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from dotenv import load_dotenv
+load_dotenv()  # This loads the environment variables from the .env file
 from flask import Flask, jsonify, request
 import requests
 from bs4 import BeautifulSoup
